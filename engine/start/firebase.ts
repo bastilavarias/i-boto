d62610b31dabee089573a646220ece950a05aee2
Sebastian Curtis T. Lavarias <sebastiancurtislavarias@gmail.com>
2025-04-16 19:34:02 +0800

    ADD: Deployment configs

--- engine/start/firebase.ts
+++ engine/start/firebase.ts
@@ -1,7 +1,6 @@
 import { initializeApp, cert } from 'firebase-admin/app'
 import { getAuth } from 'firebase-admin/auth'
-// @ts-ignore
-import serviceAccount from '../firebasedev-sk.json'
+import serviceAccount from '../firebasedev-sk.json' with { type: 'json' }
 
 initializeApp({
   credential: cert(serviceAccount as any),
