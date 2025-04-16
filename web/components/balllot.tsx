bfaf263cbc7a998fe45a68bd37a011a746bdfdbd
Sebastian Curtis T. Lavarias <sebastiancurtislavarias@gmail.com>
2025-04-16 21:43:00 +0800

    Update balllot.tsx

--- web/components/balllot.tsx
+++ web/components/balllot.tsx
@@ -17,7 +17,6 @@ import {
     AlertDialogAction,
     AlertDialogCancel,
     AlertDialogContent,
-    AlertDialogDescription,
     AlertDialogFooter,
     AlertDialogHeader,
     AlertDialogTitle,
