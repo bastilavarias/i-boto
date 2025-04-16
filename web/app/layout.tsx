ac2b139c2db80823f92f02cddd854887bde70f35
Sebastian Curtis T. Lavarias <sebastiancurtislavarias@gmail.com>
2025-04-16 21:16:30 +0800

    FIX: Added subsets

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -7,6 +7,7 @@ const sansSerif = Poppins({
     variable: '--font-poppins',
     weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
     preload: true,
+    subsets: ['latin']
 })
 
 const mono = JetBrains_Mono({
